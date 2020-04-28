1a971592b1779ee77bf546f2745bbebecdea70b9
Peter <pc.dean@icloud.com>
2020-04-28 12:37:56 +0100

    create board class and initalize with empty board

--- game.cpp
+++ game.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+class Board {
+
+  private:
+    char board;
+
+  public:
+    void dispBoard() {
+      char board[9] = {};
+      cout << "  " << 1 << ' ' << 2 << ' '<< 3 << '\n';
+      for (int i=3; i<10; i+=3) {
+        cout << i/3 << ' ';
+        cout << board[i-3] << ' ';
+        cout << board[i-2] << ' ';
+        cout << board[i-1] << '\n';
+      }
+    }
+    Board() {
+      dispBoard();
+    }
+
+};
+
 int main() {
-  
+  Board newGame;
 }
