46e24cc143f29cf6b2805e883f90e79bafce9055
Peter <pc.dean@icloud.com>
2020-04-28 13:20:40 +0100

    imporve syntax to utilize c++ 11

--- game.cpp
+++ game.cpp
@@ -4,25 +4,33 @@ using namespace std;
 class Board {
 
   private:
-    char board;
+    char board[9] = { };
 
   public:
+
+    Board() {
+      dispBoard();
+    }
+
     void dispBoard() {
-      char board[9] = {};
       cout << "  " << 1 << ' ' << 2 << ' '<< 3 << '\n';
       for (int i=3; i<10; i+=3) {
         cout << i/3 << ' ';
-        cout << board[i-3] << ' ';
-        cout << board[i-2] << ' ';
-        cout << board[i-1] << '\n';
+        cout << this->board[i-3] << ' ';
+        cout << this->board[i-2] << ' ';
+        cout << this->board[i-1] << '\n';
       }
     }
-    Board() {
-      dispBoard();
+
+    void makemove(int move) {
+      
     }
 
 };
 
 int main() {
   Board newGame;
+  cout << "Enter moves as an [x,y] coordinate array \n";
+  cout << "PlayerXs Move:\n";
+  cin >> makemove(move);
 }
