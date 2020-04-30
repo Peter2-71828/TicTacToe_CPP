0d4973efe44a48c0a92ad2a5cc2d217d8bc40c19
Peter <pc.dean@icloud.com>
2020-04-30 16:23:22 +0100

    create Board Class

--- game.cpp
+++ game.cpp
@@ -12,3 +12,23 @@ public:
     }
 
 };
+
+class Board {
+
+public:
+    char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    void dispBoard() {
+      for (int i=3; i<10; i+=3) {
+        cout << this->board[i-3] << ' ';
+        cout << this->board[i-2] << ' ';
+        cout << this->board[i-1] << '\n';
+      }
+    }
+
+    void updateBoard(int position, Player player){
+      this->board[position] = player.name;
+      dispBoard();
+    }
+
+};
