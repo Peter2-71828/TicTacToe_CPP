e97e34e3a20bd645a73f28150e4842e88c06eee9
Peter <pc.dean@icloud.com>
2020-04-30 14:47:19 +0100

    abstact functionality from board into TicTacToe

--- game.cpp
+++ game.cpp
@@ -14,20 +14,10 @@ public:
 
 class Board {
 
-  private:
+  protected:
     char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
   public:
-
-    Board() {
-      Player playerX('X');
-      Player playerO('O');
-      playerX.next = &playerO;
-      playerO.next = &playerX;
-      dispBoard();
-      cout << "Enter the number in the position you wish to play\n";
-      playerPrompt(&playerX);
-    }
 
     void dispBoard() {
       for (int i=3; i<10; i+=3) {
@@ -37,22 +27,38 @@ class Board {
       }
     }
 
-    void playerPrompt( Player* player) {
-      char move;
-      cout << "Player" << player->name << "s Move:\n";
-      cin >> move;
-      this->makeMove(move, *player);
-    }
+};
 
-    void makeMove(char move, Player player){
-      int position = move - '1';
-      this->board[position] = player.name;
-      dispBoard();
-      playerPrompt(player.next);
-    }
+class TicTacToe: public Board {
+
+  public: 
+
+  TicTacToe(){
+    Player playerX('X');
+    Player playerO('O');
+    playerX.next = &playerO;
+    playerO.next = &playerX;
+    dispBoard();
+    cout << "Enter the number in the position you wish to play\n";
+    playerPrompt(&playerX);
+  }
+
+  void playerPrompt( Player* player) {
+    char move;
+    cout << "Player" << player->name << "s Move:\n";
+    cin >> move;
+    this->makeMove(move, *player);
+  }
+
+  void makeMove(char move, Player player){
+    int position = move - '1';
+    this->board[position] = player.name;
+    dispBoard();
+    playerPrompt(player.next);
+  }
 
 };
 
 int main() {
-  Board newGame;
+  TicTacToe newGame;
 }
